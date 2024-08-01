184d933e039718d55c436f478907514c96e1257d
Your Name <trungduongnguyen2004@gmail.com>
2024-07-31 19:19:30 -0700

    get message

--- backend/controllers/message.controlers.js
+++ backend/controllers/message.controlers.js
@@ -1,45 +1,66 @@
 import Conversation from "../models/conversation.model.js";
-import Message from "../models/message.model.js"
+import Message from "../models/message.model.js";
 export const sendMessage = async (req, res) => {
-    
-    try{
-        //get user id and message content from req
-        const {message} = req.body;
-        const {id:receiverId} = req.params;
-        const senderId = req.user._id;
-        
-        //find consversation between 2 user
-        let conversation = await Conversation.findOne({
-            participants: {$all: [senderId, receiverId]},
-        })
-        //if didn't exist, create new one
-        if (!conversation) {
-            conversation = await Conversation.create({
-                participants: [senderId, receiverId]
-            })
-        }
+  try {
+    //get user id and message content from req
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-        //create new message
-        const newMessage = new Message({
-            senderID: senderId,
-            receiverID: receiverId,
-            message: message
-        })
-        //put new message into conversation
-        if (newMessage) {
-            conversation.messages.push(newMessage._id);
-        }
-
-		//save stuff into database synchronously
-		// await conversation.save()
-		// await newMessage.save()
-
-		// save into database parralelly
-		await Promise.all([conversation.save(), newMessage.save()])
-
-        res.status(201).json({newMessage})
-    }catch (error) {
-        console.log("Error in sendMessage: ", error.message)
-        res.status(500).json({error: "Internal sever error in sendMessage"})
+    //find consversation between 2 user
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+    //if didn't exist, create new one
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
     }
-}
+
+    //create new message
+    const newMessage = new Message({
+      senderID: senderId,
+      receiverID: receiverId,
+      message: message,
+    });
+    //put new message into conversation
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+    //todo: socket io
+
+    //save stuff into database synchronously
+    // await conversation.save()
+    // await newMessage.save()
+
+    // save into database parralelly
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    res.status(201).json({ newMessage });
+  } catch (error) {
+    console.log("Error in sendMessage: ", error.message);
+    res.status(500).json({ error: "Internal sever error in sendMessage" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); //populate message convert message id to content
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessage: ", error.message);
+    res.status(500).json({ error: "Internal sever error in getMessage" });
+  }
+};
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controlers.js";
+import { sendMessage, getMessages } from "../controllers/message.controlers.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 //run protectRoute first, if run successfully, run sendMessage (see protectRoute implement)
-router.post("/send/:id", protectRoute, sendMessage)
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
