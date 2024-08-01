e4373e9171af2f7a69b5812c9d9ff681b9b7becc
Your Name <trungduongnguyen2004@gmail.com>
2024-07-31 19:53:21 -0700

    able to get all of the user except for the one that is logging in

--- backend/sever.js
+++ backend/sever.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -16,6 +17,7 @@ app.use(cookieParser()); // to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //     //root route http://localhost:5000/
