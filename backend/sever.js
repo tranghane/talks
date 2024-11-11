fcab06bfaaeb8c6bfcb71c9844ce0f3e48fca302
Your Name <trungduongnguyen2004@gmail.com>
2024-11-11 15:58:33 -0800

    for deployment

--- backend/sever.js
+++ backend/sever.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,6 +15,8 @@ dotenv.config(); //allow using environment variable
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //to parse the incoming requests with Json payloads (from req.body)
 app.use(cookieParser()); // to parse incoming cookies
 
@@ -21,11 +24,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
-//     res.send("Is this thing on?")
-//     // res.send("Hello?")
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+  
 
 server.listen(PORT, () => {
   connectToMongoDB();
